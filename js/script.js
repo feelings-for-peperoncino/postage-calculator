41f75ffac553913043dce37a3f8eceaa55cf0e45
feelings-for-peperoncino <87107270+feelings-for-peperoncino@users.noreply.github.com>
2023-09-10 11:26:51 +0900

    更新
    コードを再編しました

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// 金額をクリックしたときの処理
 const priceElements = document.querySelectorAll('.price');
 const quantityInputs = document.querySelectorAll('.quantity-input');
 
@@ -9,7 +8,6 @@ priceElements.forEach((priceElement, index) => {
 });
 
 quantityInputs.forEach((quantityInput, index) => {
-  // ユーザーが入力欄をクリックしたら、0をクリアする
   quantityInput.addEventListener('click', () => {
     if (quantityInput.value === '0') {
       quantityInput.value = '';
@@ -28,31 +26,22 @@ function updateTotal(index) {
   const totalQuantityElement = document.querySelector('.total-quantity');
   const grandTotalElement = document.querySelector('.grand-total');
 
-  // 金額の値を取得
   const priceValue = parseFloat(priceElement.getAttribute('data-value'));
-
-  // 通数を取得し、空白の場合は0に設定
   const quantity = parseInt(quantityElement.value) || 0;
-
-  // 合計金額を計算
   const total = priceValue * quantity;
 
-  // 金額を表示する際に小数点以下の ".00" を削除して表示
   totalElement.textContent = total === Math.floor(total) ? total + '円' : total.toFixed(2) + '円';
 
-  // 合計通数を更新
   let totalQuantity = 0;
   quantityInputs.forEach((input) => {
     totalQuantity += parseInt(input.value) || 0;
   });
   totalQuantityElement.textContent = totalQuantity;
 
-  // 合計金額を更新
   let grandTotal = 0;
   priceElements.forEach((element, i) => {
     grandTotal += parseFloat(element.parentElement.querySelector('.total').textContent);
   });
 
-  // 金額を表示する際に小数点以下の ".00" を削除して表示
   grandTotalElement.textContent = grandTotal === Math.floor(grandTotal) ? grandTotal + '円' : grandTotal.toFixed(2) + '円';
 }
